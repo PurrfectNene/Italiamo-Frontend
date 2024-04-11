ba68c97f1d427f276d82bcb1caa729b906922ded
Eduardo Botelho <eduardo11224b@gmail.com>
2024-04-11 18:09:19 +0100

    Removing styles and doing login form states

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+
 export default function LoginPage() {
-  return <h1>LoginPage</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  console.log(email, password);
+
+  return (
+    <div>
+      <label htmlFor="email">Email</label>
+      <input
+        type="text"
+        id="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        type="text"
+        id="password"
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+    </div>
+  );
 }
