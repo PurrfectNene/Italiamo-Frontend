eca9c2f93ed1de5d1d5b29c321296d2f45f9002b
Eduardo Botelho <eduardo11224b@gmail.com>
2024-04-18 18:32:11 +0100

    navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,23 @@
-import { Flex } from "antd";
+import { Flex, Space } from "antd";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
+import "./Navbar.css";
 
 export default function Navbar() {
+  const { isLoggedIn } = useContext(AuthContext);
+
+  const items = [
+    {
+      key: 0,
+      label: <Link to={"/login"}>Login</Link>,
+    },
+    {
+      key: 1,
+      label: <Link to={"/register"}>Register</Link>,
+    },
+  ];
+
   return (
     <Flex
       component="nav"
@@ -8,12 +25,101 @@ export default function Navbar() {
         minHeight: 60,
         backgroundColor: "white",
         boxShadow: "0 2px 4px -1px rgba(57,76,96,.15)",
-
         zIndex: 100,
         position: "relative",
+        alignItems: "center",
+        display: "flex",
+        paddingLeft: "1.5rem",
+        paddingRight: "1.5rem",
       }}
     >
-      Item 1
+      <Space
+        style={{
+          fontSize: "1.1rem",
+          marginLeft: "auto",
+          marginRight: "3rem",
+        }}
+        size={"large"}
+      >
+        <Link className="menu-link" to="/regions">
+          Where to go
+        </Link>
+        <Link className="menu-link" to="/cities">
+          What to do
+        </Link>
+      </Space>
+      <span
+        className="roboto-slab"
+        style={{
+          fontSize: "2rem",
+          color: "#5f4e44",
+        }}
+      >
+        ITALIAMO
+      </span>
+      <Space
+        style={{
+          fontSize: "1.1rem",
+          marginRight: "auto",
+          marginLeft: "3rem",
+        }}
+        size={"large"}
+      >
+        <Link className="menu-link" to="/login">
+          Login
+        </Link>
+        <Link className="menu-link" to="/register">
+          Create account
+        </Link>
+      </Space>
+      {/* {isLoggedIn ? (
+        <></>
+      ) : (
+        <div
+          style={{
+            marginLeft: "auto",
+            display: "flex",
+            gap: 0,
+          }}
+        >
+          <Button
+            type="text"
+            style={{
+              marginLeft: "auto",
+              display: "flex",
+              gap: "0.75rem",
+              fontSize: "1.25rem",
+              color: "#5f4e44",
+              padding: "0.5rem",
+              height: "auto",
+            }}
+          >
+            <SearchOutlined />
+          </Button>
+          <Dropdown
+            menu={{
+              items,
+            }}
+            placement="bottomLeft"
+            trigger="click"
+          >
+            <Button
+              type="text"
+              style={{
+                marginLeft: "auto",
+                display: "flex",
+                gap: "0.75rem",
+                fontSize: "1.25rem",
+                color: "#5f4e44",
+                padding: "0.5rem",
+                height: "auto",
+              }}
+            >
+              <UserOutlined />
+            </Button>
+          </Dropdown>
+        </div>
+      )} */}
     </Flex>
   );
 }
